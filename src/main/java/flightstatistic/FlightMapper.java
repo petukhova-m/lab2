3746adc252ba06206ce28d1bb8962aadd97990a8
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 08:57:33 +0300

    Scripted auto-commit on change (2021-12-17 08:57:33) by gitwatch.sh

--- src/main/java/flightstatistic/FlightMapper.java
+++ src/main/java/flightstatistic/FlightMapper.java
@@ -9,7 +9,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComp
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (key.get() == 0) {
             return;
-
+        }
         DataWritable writable = new DataWritable(value.toString(), EntityType.FLIGHT);
         if (writable.getDelay() > Constants.ZERO && !writable.isCancelled()) {
             context.write(new AirportWritableComparable(writable.getId(), EntityType.FLIGHT), writable);
