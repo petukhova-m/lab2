e8a74e2719d5a3180f1e2cabe6143a5725e62871
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 08:05:27 +0300

    Scripted auto-commit on change (2021-12-17 08:05:27) by gitwatch.sh

--- src/main/java/flightstatistic/FlightMapper.java
+++ src/main/java/flightstatistic/FlightMapper.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComparable, DataWritable> {
     @Override
 
-
+        if (key.get() == 0) {
 
 
 
