3ba2150e7896b0ed566cab01c203901b742778b8
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 08:55:32 +0300

    Scripted auto-commit on change (2021-12-17 08:55:32) by gitwatch.sh

--- src/main/java/flightstatistic/AirportMapper.java
+++ src/main/java/flightstatistic/AirportMapper.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableComparable, DataWritable> {
     @Override
-    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
 
             return;
         }
