ec5aef8a7cbe38665b8f9db9f4fe6506fc3bcea2
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 08:56:54 +0300

    Scripted auto-commit on change (2021-12-17 08:56:54) by gitwatch.sh

--- src/main/java/flightstatistic/FlightPartitioner.java
+++ src/main/java/flightstatistic/FlightPartitioner.java
@@ -4,7 +4,7 @@ import org.apache.hadoop.io.Text;
 
 public class FlightPartitioner extends Partitioner<AirportWritableComparable, DataWritable> {
     @Override
-
+    public int getPartition(AirportWritableComparable key, DataWritable value, int numPartitions) {
         return key.getId() % numPartitions;
     }
 }
