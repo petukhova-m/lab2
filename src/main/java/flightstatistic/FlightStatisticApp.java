c74d8659221b36ec92c8afc06e003fe2f2a56913
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 08:52:15 +0300

    Scripted auto-commit on change (2021-12-17 08:52:15) by gitwatch.sh

--- src/main/java/flightstatistic/FlightStatisticApp.java
+++ src/main/java/flightstatistic/FlightStatisticApp.java
@@ -1,7 +1,7 @@
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
-
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
