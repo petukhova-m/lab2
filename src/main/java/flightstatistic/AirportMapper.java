1a2825dbfaf0ffb75701c9bd11e48b4fa5e50d2c
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 07:58:46 +0300

    Scripted auto-commit on change (2021-12-17 07:58:46) by gitwatch.sh

--- src/main/java/flightstatistic/AirportMapper.java
+++ src/main/java/flightstatistic/AirportMapper.java
@@ -7,7 +7,7 @@ public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableCom
 
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
-
+            return;
 
         DataWritable writable = new DataWritable(value.toString(), EntityType.AIRPORT);
 
