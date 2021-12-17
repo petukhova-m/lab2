be6c074ddbc49eb6de32f0b948b34a05a3dfc4f2
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 08:59:12 +0300

    Scripted auto-commit on change (2021-12-17 08:59:12) by gitwatch.sh

--- src/main/java/flightstatistic/AirportMapper.java
+++ src/main/java/flightstatistic/AirportMapper.java
@@ -1,5 +1,5 @@
 package flightstatistic;
-import java.io.IOException;
+
 
 import org.apache.hadoop.mapreduce.Mapper;
 
@@ -9,7 +9,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 
             return;
         }
-        DataWritable writable = new DataWritable(value.toString(), EntityType.AIRPORT);
-        context.write(new AirportWritableComparable(writable.getId(), EntityType.AIRPORT), writable);
 
+        context.write(new AirportWritableComparable(writable.getId(), EntityType.AIRPORT), writable);
+    }
 }
--- src/main/java/flightstatistic/Constants.java
+++ src/main/java/flightstatistic/Constants.java
@@ -4,6 +4,6 @@ package flightstatistic;
     public static final int AIRPORT_ID = 14;
     public static final int ZERO = 0;
 
-    public static final int DESCRIPTION = 1;
+
     public static final int CODE = 0;
 }
