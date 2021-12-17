05ab0f732c75b0e5e898eef26197f2ac35c78090
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 09:01:03 +0300

    Scripted auto-commit on change (2021-12-17 09:01:03) by gitwatch.sh

--- src/main/java/flightstatistic/FlightComparator.java
+++ src/main/java/flightstatistic/FlightComparator.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.io.WritableComparator;
 
 public class FlightComparator extends WritableComparator {
     public FlightComparator() {
-
+        super (AirportWritableComparable.class, true);
     }
     @Override
     public int compare(WritableComparable a, WritableComparable b) {
