7b6560d3770031ef2228e2fe092d73c49956ca9d
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 08:01:49 +0300

    Scripted auto-commit on change (2021-12-17 08:01:49) by gitwatch.sh

--- src/main/java/flightstatistic/AirportMapper.java
+++ src/main/java/flightstatistic/AirportMapper.java
@@ -11,5 +11,5 @@ public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableCom
 
         DataWritable writable = new DataWritable(value.toString(), EntityType.AIRPORT);
         context.write(new AirportWritableComparable(writable.getId(), EntityType.AIRPORT), writable);
-
+    }
 }
