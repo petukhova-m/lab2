7ee7f4cdb16d3e03ea2289f6ad87250345aed35b
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 08:57:03 +0300

    Scripted auto-commit on change (2021-12-17 08:57:03) by gitwatch.sh

--- src/main/java/flightstatistic/AirportMapper.java
+++ src/main/java/flightstatistic/AirportMapper.java
@@ -1,11 +1,11 @@
-package flightstatistic;
 
+import java.io.IOException;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Mapper;
 
 
     @Override
-
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (key.get() == 0) {
 
         }
--- src/main/java/flightstatistic/Constants.java
+++ src/main/java/flightstatistic/Constants.java
@@ -1,5 +1,5 @@
 package flightstatistic;
-
+public class Constants {
     public static final int ARR_DELAY = 18;
     public static final int AIRPORT_ID = 14;
     public static final int ZERO = 0;
