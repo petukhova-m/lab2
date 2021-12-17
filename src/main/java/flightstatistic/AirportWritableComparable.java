24fc52b08c62999f17565a4fe6045acf128f56a3
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 08:16:19 +0300

    Scripted auto-commit on change (2021-12-17 08:16:19) by gitwatch.sh

--- src/main/java/flightstatistic/AirportWritableComparable.java
+++ src/main/java/flightstatistic/AirportWritableComparable.java
@@ -19,7 +19,7 @@ public class AirportWritableComparable implements WritableComparable<AirportWrit
     public int getId() {
         return id;
     }
-
+    @Override
     public void readFields(DataInput in) throws IOException {
         this.id = in.readInt();
         this.type = EntityType.values()[in.readInt()];
