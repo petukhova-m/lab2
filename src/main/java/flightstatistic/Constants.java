7f53e55b023ffd3e74a078c7ab45c348f942df4f
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 08:59:41 +0300

    Scripted auto-commit on change (2021-12-17 08:59:41) by gitwatch.sh

--- src/main/java/flightstatistic/Constants.java
+++ src/main/java/flightstatistic/Constants.java
@@ -1,6 +1,6 @@
 package flightstatistic;
 public class Constants {
-    public static final int ARR_DELAY = 18;
+
     public static final int AIRPORT_ID = 14;
     public static final int ZERO = 0;
 
