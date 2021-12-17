625843e4f2822ad0a4a874ca9013fcdf853804b5
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 08:21:44 +0300

    Scripted auto-commit on change (2021-12-17 08:21:44) by gitwatch.sh

--- src/main/java/flightstatistic/DataWritable.java
+++ src/main/java/flightstatistic/DataWritable.java
@@ -29,7 +29,7 @@ public class DataWritable implements Writable {
 
 
             this.id = Integer.parseInt(data[Constants.AIRPORT_ID].replaceAll("\"", ""));
-
+            this.delay = data[Constants.ARR_DELAY].length() > 0 ? Float.parseFloat(data[Constants.ARR_DELAY]) : 0;
             this.cancelled = Float.parseFloat(data[Constants.IS_CANCELED]) == 1;
         }
 
