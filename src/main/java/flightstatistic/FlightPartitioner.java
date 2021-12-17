331c96a5a31f6213787d2ea13f40634571b21619
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 09:00:33 +0300

    Scripted auto-commit on change (2021-12-17 09:00:33) by gitwatch.sh

--- src/main/java/flightstatistic/FlightPartitioner.java
+++ src/main/java/flightstatistic/FlightPartitioner.java
@@ -2,7 +2,7 @@
 import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.io.Text;
 
-public class FlightPartitioner extends Partitioner<AirportWritableComparable, DataWritable> {
+
     @Override
     public int getPartition(AirportWritableComparable key, DataWritable value, int numPartitions) {
 
