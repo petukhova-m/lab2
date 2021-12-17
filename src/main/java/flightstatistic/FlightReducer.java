e9192cdaf3fadfd41eaa35b1c4f968833e5ae331
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 09:05:31 +0300

    Scripted auto-commit on change (2021-12-17 09:05:31) by gitwatch.sh

--- src/main/java/flightstatistic/FlightReducer.java
+++ src/main/java/flightstatistic/FlightReducer.java
@@ -23,7 +23,7 @@ public class FlightReducer extends Reducer<AirportWritableComparable, DataWritab
             average += arrDelay;
             min = Math.min(arrDelay, min);
             max = Math.max(arrDelay, max);
-
+        }
         if (counter > Constants.ZERO) {
             context.write(key.getId(), new Text(String.format(pattern, name, min, max, average / counter)));
         }
