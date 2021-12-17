22101b1f98a41df6e2cdbbb059521a865bf898fd
petukhova-m <petukhova-01@yandex.ru>
2021-12-17 08:04:38 +0300

    Scripted auto-commit on change (2021-12-17 08:04:38) by gitwatch.sh

--- src/main/java/flightstatistic/Constants.java
+++ src/main/java/flightstatistic/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
     public static final int AIRPORT_ID = 14;
     public static final int ZERO = 0;
     public static final int IS_CANCELED = 19;
-
+    public static final int DESCRIPTION = 1;
     public static final int CODE = 0;
 }
